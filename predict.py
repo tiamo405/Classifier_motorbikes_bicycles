6ac0f6b734f0e1ea421014023efdd2fa895126c2
tiamo405 <nam05052002@gmail.com>
2023-10-09 14:56:39 +0700

    add model vgg16

--- predict.py
+++ predict.py
@@ -10,16 +10,21 @@ from torchvision import transforms
 from config import config
 
 from utils import utils_save_cfg, utils_model, utils_loss
+import random
 
 class Model():
     
     def __init__(self, load_height, load_width ,
                  checkpoint_dir, device):
 
-        self.model = torchvision.models.resnet101(pretrained = False)
+        # self.model = torchvision.models.resnet101(pretrained = False)
         # self.model = utils_model.create_model(name_model= 'resnet50', num_classes= 2)
-        num_features = self.model.fc.in_features
-        self.model.fc = nn.Linear(num_features, 2) # khởi tạo mô hình = trẻ 3 tuổi
+        # num_features = self.model.fc.in_features
+        # self.model.fc = nn.Linear(num_features, 2) # khởi tạo mô hình = trẻ 3 tuổi
+
+        self.model = torchvision.models.vgg16()
+        num_features = self.model.classifier[6].in_features
+        self.model.classifier[6] = nn.Linear(num_features, 2)
         self.device = device # gán biến
         self.model.to(self.device)
 
@@ -42,6 +47,7 @@ class Model():
         
 
     def preprocess(self, path_image):
+
         img = cv2.imread(path_image) # đọc ảnh
         img  = self.transform(img) # chuyển ảnh về dangh tensor
         return img.to(self.device).unsqueeze(0) # thêm 1 chiều cho ảnh 1,n,h,w thì phải # 1*3*224*224
@@ -72,18 +78,54 @@ def main(args):
     LOAD_HEIGHT = cfg['LOAD_HEIGHT']
     IMAGE = args.image
     
+    dict_class = {'xedap': 0, 'xemay' : 1}
+    folder_xedap = os.path.join(args.dir_root, 'test/xedap')
+    folder_xemay = os.path.join(args.dir_root, 'test/xemay')
+
+    # Lấy danh sách các tệp tin trong thư mục xedap và xemay
+    images_xedap = [os.path.join(folder_xedap, filename) for filename in os.listdir(folder_xedap) if filename.endswith('.jpg')]
+    images_xemay = [os.path.join(folder_xemay, filename) for filename in os.listdir(folder_xemay) if filename.endswith('.jpg')]
+
+    # Kết hợp danh sách ảnh từ hai thư mục
+    all_images = images_xedap + images_xemay
+    # print(all_images)
+    # Sắp xếp ngẫu nhiên danh sách ảnh
+    # random.shuffle(all_images)
+    # print(all_images)
     model = Model(load_height= LOAD_HEIGHT, load_width= LOAD_WIDTH,\
                   checkpoint_dir= CHECKPOINT_DIR, device= DEVICE)
-    label, score = model.predict(IMAGE)
+    gt_labels = []
+    pre_labels = []
 
-    print('path_image: {} \nlabel : {} \nxac xuat: {}'.format(IMAGE, label, score))
+    for i, image in enumerate(all_images):
+        label, score = model.predict(image)
+        pre_labels.append(dict_class[label])
+        gt_label = dict_class.get(os.path.basename(os.path.dirname(image)), -1)
+        gt_labels.append(gt_label)
+        # print(image, label)
+    # print('path_image: {} \nlabel : {} \nxac xuat: {}'.format(IMAGE, label, score))
 
+    # Tổng số mẫu
+    total_samples = len(gt_labels)
+
+    # Số lần dự đoán đúng
+    correct_predictions = sum(1 for true_label, pred_label in zip(gt_labels, pre_labels) if true_label == pred_label)
+
+    # Độ chính xác
+    accuracy = correct_predictions / total_samples
+
+    # Độ chính xác sẽ là một số từ 0 đến 1, hoặc có thể nhân 100 để biểu diễn dưới dạng phần trăm.
+    accuracy_percentage = accuracy * 100
+
+    print(f"Độ chính xác của mô hình là: {accuracy_percentage:.2f}%")
     
 def get_args_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', type= str, default= 'test')
-    parser.add_argument('--checkpoint_dir', type=str, default='checkpoint/resnet101/2023-09-27/5.pth')
-    parser.add_argument('--image', type= str, default= 'data/test/xedap/xedap_0992.jpg')
+    parser.add_argument('--checkpoint_dir', type=str, default='checkpoint/vgg16/2023-09-29/20.pth')
+    parser.add_argument('--image', type= str, default= 'photo_2023-10-02_16-53-58.jpg')
+    parser.add_argument('--dir_root', type= str, default= '/root')
+
     opt = parser.parse_args()
     return opt
 
